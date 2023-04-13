73c0c415acc55e5503857b14e6fe091e0ecce3a1
Kst <andrewsergeev98@gmil.com>
2023-04-13 13:41:43 +0300

    Initial commit

---
+++ src/main/java/com/sergeev/application/Main.java
@@ -0,0 +1,13 @@
+package com.sergeev.application;
+
+/**
+ * The class contains a main method!
+ */
+public class Main {
+    /**
+     * The method is the entry point for a running program
+     *
+     * @param arguments arguments are command line arguments that you can specify.
+     */
+    public static void main(String[] arguments) {}
+}
